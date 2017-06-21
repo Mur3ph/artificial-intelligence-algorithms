42f3e1e2e0c60ac4e4cc962bb6006dbb9836abcf
Mur3ph <paulmurphymainstreet@gmail.com>
2017-06-21 22:06:17 +0100

    Initial commit..21-06-2017

---
+++ src/ie/murph/main/knearest/neighbors/RunKNearestNeighborsAlgorithm.java
@@ -0,0 +1,22 @@
+package ie.murph.main.knearest.neighbors;
+
+public class RunKNearestNeighborsAlgorithm {
+	private Integer[] dataset = {3,5,678,8,99};
+	private Integer[] sample = {1,2,4,6,87};
+
+	public static void main(String[] args){
+		
+	}
+	
+//	Java Implementation - http://afewguyscoding.com/2010/05/nearest-neighbors-java/
+	
+//	Pseudocode - https://stackoverflow.com/questions/22796864/k-nearest-neighbor-pseudocode
+	
+//	kNN (dataset, sample){
+//		   1. Go through each item in my dataset, and calculate the "distance" 
+//		   from that data item to my specific sample.
+//		   2. Classify the sample as the majority class between K samples in 
+//		   the dataset having minimum distance to the sample.
+//		}
+	
+}
