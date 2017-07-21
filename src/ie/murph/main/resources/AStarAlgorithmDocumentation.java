dcc3f9670dfd139129fb589118d4c9d9e253049a
Mur3ph <paulmurphymainstreet@gmail.com>
2017-07-21 22:22:56 +0100

    more resources

--- src/ie/murph/main/resources/AStarAlgorithmDocumentation.java
+++ src/ie/murph/main/resources/AStarAlgorithmDocumentation.java
@@ -6,6 +6,8 @@ public class AStarAlgorithmDocumentation {
 		System.out.println("A* Java code Resource:  https://codereview.stackexchange.com/questions/38376/a-search-algorithm");
 		System.out.println("Pseudocode wiki:        https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode");
 		System.out.println("Youtube video: 	        https://www.youtube.com/watch?v=DhtSZhakyOo");
+		System.out.println("Youtube playlist:       https://www.youtube.com/channel/UCWN3xxRkmTPmbKwht9FuE5A");
+		System.out.println("Youtube statistics:     https://www.youtube.com/watch?v=uhxtUt_-GyM&list=PL1328115D3D8A2566");
 	}
 	
 }
